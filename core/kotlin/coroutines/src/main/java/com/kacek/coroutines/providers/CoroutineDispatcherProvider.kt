9f00cf322492a88e2351dd0611d8bf26c3cfe16e
Kacper Kalinowski <1kapka2@gmail.com>
2024-08-01 16:08:52 +0200

    Initial network implementation (#3)
    * add initial implementation of network client
    
    * clean unused version in libs
    
    * fall back from deciding on out generic yet

---
+++ core/kotlin/coroutines/src/main/java/com/kacek/coroutines/providers/CoroutineDispatcherProvider.kt
@@ -0,0 +1,11 @@
+package com.kacek.coroutines.providers
+
+import kotlinx.coroutines.Dispatchers
+import kotlin.coroutines.CoroutineContext
+
+internal class CoroutineDispatcherProvider : DispatcherProvider {
+    override val default: CoroutineContext get() = Dispatchers.Default
+    override val main: CoroutineContext get() = Dispatchers.Main
+    override val io: CoroutineContext get() = Dispatchers.IO
+    override val unconfined: CoroutineContext get() = Dispatchers.Unconfined
+}
--- core/network/build.gradle.kts
+++ core/network/build.gradle.kts
@@ -1,3 +1,13 @@
 plugins {
     id("kotlin")
 }
+
+dependencies {
+    implementation(project(":core:kotlin:coroutines"))
+
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.okhttp)
+    implementation(libs.ktor.client.logging)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.client.serialization.kotlinx.json)
+}
