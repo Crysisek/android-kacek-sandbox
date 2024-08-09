4c95d516903aff291fddf02674d52bc34f6e8488
Kacper Kalinowski <1kapka2@gmail.com>
2024-08-09 13:56:11 +0200

    Add TODOS.md (#11)

--- app/src/main/java/com/kacek/sandbox/MainViewModel.kt
+++ app/src/main/java/com/kacek/sandbox/MainViewModel.kt
@@ -11,14 +11,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(): ViewModel() {
-    /* When [Enable K2 Kotlin mode] option in Android Studio will be more stable
-     * then we will be able to use the following syntax:
-     *
-     *  val isAppReady: StateFlow<Boolean>
-     *      field = MutableStateFlow(false)
-     *
-     * https://github.com/Kotlin/KEEP/blob/explicit-backing-fields-re/proposals/explicit-backing-fields.md
-     */
+    // TODO-1
     private val _isAppReady = MutableStateFlow(false)
     val isAppReady = _isAppReady.asStateFlow()
 
